c7b2dfa3445b8d73f9053e6b2cb36a5b1199c392
gablax <52874630+gablax@users.noreply.github.com>
2022-12-07 09:28:06 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+
+<div class="container  bg_danger">
+<?php
+
+get_header();
+if(have_posts()):
+    while(have_posts()): the_post();
+?>
+<h1> <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+<p> <?php  the_content();  ?> </p>
+<hr>
+
+<?php
+endwhile;
+endif;
+
+get_footer();
+
+
+?>
+</div>
